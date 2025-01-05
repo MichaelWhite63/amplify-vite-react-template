f5c210846f5b900641657d76454c71ee625438aa
Michael White <michaelwhite63@gmail.com>
2025-01-05 17:55:58 -0500

    Added createUser but I don't beleive it is correct

--- amplify/functions/create-user/handler.ts
+++ amplify/functions/create-user/handler.ts
@@ -1,7 +1,8 @@
-import { AdminCreateUserCommand } from '@aws-sdk/client-cognito-identity-provider';
-import { cognitoClient } from './resource';
 import type { Schema } from "../../data/resource";
 export const USER_POOL_ID = process.env.USER_POOL_ID || 'us-east-1_oy1KeDlsD';
+/*
+import { AdminCreateUserCommand } from '@aws-sdk/client-cognito-identity-provider';
+import { cognitoClient } from './resource';
 
 interface UserAttributes {
   Name: string;
@@ -12,11 +13,13 @@ interface CreateUserResponse {
   User?: {
     Username?: string;
   };
-}
+}*/
 
 export const handler: Schema["createUser"]["functionHandler"] = async (event) => {
   const { email, password } = event.arguments as { email: string; password: string };
 
+  return (USER_POOL_ID + " " + email + " " + password);
+/*
   const createUserCommand = new AdminCreateUserCommand({
     UserPoolId: USER_POOL_ID,
     Username: email,
@@ -40,4 +43,5 @@ export const handler: Schema["createUser"]["functionHandler"] = async (event) =>
     message: 'User created successfully',
     username: response.User?.Username,
   });
+  */
 };
